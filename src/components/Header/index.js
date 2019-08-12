a3e6d4cd753fb6859255102430c4b758902956f3
Mathieu Marques <mathieumarques78@gmail.com>
2019-08-13 01:17:22 +0200

    Fix header spacing

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,16 +1,29 @@
 import AppBar from '@material-ui/core/AppBar';
-import Box from '@material-ui/core/Box';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
 import React from 'react';
 import Search from '../Search';
 
 
+const useStyles = makeStyles(theme => ({
+  root: {
+    '& > :not(:last-child)': {
+      marginRight: theme.spacing(3),
+      [theme.breakpoints.down('xs')]: {
+        marginRight: theme.spacing(2),
+      },
+    },
+  },
+}));
+
+
 export default function Header() {
+  const classes = useStyles();
   return (
     <AppBar component="header" position="relative">
-      <Toolbar>
-        <Box children={<Typography children="The Mana Vault" variant="h6" />} clone mr={2} />
+      <Toolbar classes={{root: classes.root}}>
+        <Typography children="The Mana Vault" variant="h6" />
         <Search />
       </Toolbar>
     </AppBar>
