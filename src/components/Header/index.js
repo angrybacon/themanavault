8b41fe5ac02c9bb82371ba5cc479089b8667f73d
Mathieu Marques <mathieumarques78@gmail.com>
2019-09-01 22:03:15 +0200

    Hide header title in mobile

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,7 @@
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 import { makeStyles } from '@material-ui/core/styles';
 import React from 'react';
 import Search from '../Search';
@@ -19,11 +20,12 @@ const useStyles = makeStyles(theme => ({
 
 
 export default function Header() {
+  const desktop = useMediaQuery(theme => theme.breakpoints.up('sm'));
   const classes = useStyles();
   return (
     <AppBar component="header" position="relative">
       <Toolbar classes={{root: classes.root}}>
-        <Typography children="The Mana Vault" variant="h6" />
+        {desktop && <Typography children="The Mana Vault" variant="h6" />}
         <Search />
       </Toolbar>
     </AppBar>
